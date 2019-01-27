9b630c0f355ae8a3a1c2ddb06c77dc741bf63667
Vadim231 <40603603+Vadim231@users.noreply.github.com>
2019-01-27 21:15:19 +0600

    Add files via upload

---
+++ Hw7/Hw7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ShopContext())
+            {
+                Category category = new Category
+                {
+                    Name = "vegetables"
+                };
+
+                var user = new User
+                {
+                    Login = "vadim",
+                    Password = "password"
+                };
+
+                var cart = new Cart
+                {
+
+                };
+
+                user.Cart = cart;
+
+                var item = new Product
+                {
+                    Name = "Carrot",
+                    Price = 150
+                };
+
+                item.Category = category;
+
+                cart.Items = new List<Product>
+                {
+                    item
+                };
+
+                context.Carts.Add(cart);
+                context.Users.Add(user);
+                context.Products.Add(item);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Hw7/Hw7/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿namespace Hw7
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class ShopContext : DbContext
+    {
+        public ShopContext()
+            : base("name=ShopContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+    
+}
